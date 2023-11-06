c08c841dfa06d2815053d8a9132d8feab314ba3c
Daviti Didbaridze <ddidbaridze@gmail.com>
2023-11-06 15:15:03 +0100

    update favicon and image skeleton

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Card, CardBody, Skeleton, SkeletonText, Stack } from '@chakra-ui/react'
 const GameCardSkeleton = () => {
   return (
     <Card>
+      <Skeleton height='200px' borderRadius='1rem' />
       <CardBody>
-        <Skeleton height='200px' borderRadius='1rem' />
         <Stack mt='5' spacing='3'>
           <SkeletonText />
         </Stack>
