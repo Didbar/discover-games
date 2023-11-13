8991d463582b2aff8db3414705e3c1126c04b7e4
Daviti Didbaridze <ddidbaridze@gmail.com>
2023-11-13 23:05:16 +0100

    Refactor- create lookup hooks

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -20,8 +20,11 @@ class APIClient<T> {
     this.endpoint = endpoint
   }
 
-  getAll = (config: AxiosRequestConfig) =>
-    axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data)
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then(res => res.data)
+  }
 }
 
 export default APIClient
